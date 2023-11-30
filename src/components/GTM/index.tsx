a78e767552eb032bd0b41b035a6c563dc7abde85
pattyheiden <patriciasilvalimah@gmail.com>
2023-11-30 15:37:15 -0300

    gtm

--- src/components/GTM/index.tsx
+++ src/components/GTM/index.tsx
@@ -1,5 +1,3 @@
-const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
-
 export function GTM() {
     return (
         <>
@@ -10,7 +8,7 @@ export function GTM() {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
+          })(window,document,'script','dataLayer','GTM-PLL2K4ZQ');
         `,
                 }}
             /> 
